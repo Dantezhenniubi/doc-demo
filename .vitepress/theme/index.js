302a7642029cdeaae277228ed7a1d8c98cb7ac42
xuchongzhi <983197503@qq.com>
2025-07-30 17:58:47 +0800

    fix: 尝试修改引入顺序进行样式修复

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,9 +1,8 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue';
-
 import DefaultTheme from 'vitepress/theme';
-import './style.css';
 import './css/tailwind.css'; //tailwindcss 引入
+import './style.css';
 import TailwindCard from './components/TailwindCard.vue';
 
 // import 'bulma-prefix/css/bulma.prefixed.min.css'
