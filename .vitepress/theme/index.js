51aa1672b744604da8bf410b885958ec70fe95c9
xuchongzhi <983197503@qq.com>
2025-08-04 20:32:29 +0800

    fix: 改为引入vitepress无字体主题，尝试更换默认字体

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,7 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue';
-import DefaultTheme from 'vitepress/theme';
+// import DefaultTheme from 'vitepress/theme';
+import DefaultTheme from 'vitepress/theme-without-fonts'; // 避免在构建输出中包含 Inter字体
 import './style.css';
 import './css/tailwind.css'; //tailwindcss 引入
 import TailwindCard from './components/TailwindCard.vue';
