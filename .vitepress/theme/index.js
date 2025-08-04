148a7c1748217c762ab6db5f03cebc2ff981c2f3
xuchongzhi <983197503@qq.com>
2025-08-04 20:54:47 +0800

    fix: 全局字体单独引入，分离tailwindcssV4

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -4,6 +4,7 @@ import { h } from 'vue';
 import DefaultTheme from 'vitepress/theme-without-fonts'; // 避免在构建输出中包含 Inter字体
 import './style.css';
 import './css/tailwind.css'; //tailwindcss 引入
+import './css/vitepress-font.css'; // 引入自定义字体
 import TailwindCard from './components/TailwindCard.vue';
 import Linkcard from './components/Linkcard.vue';
 
